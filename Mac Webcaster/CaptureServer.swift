02ea93b928dc0d44c94b9b2326c95d3818742ea9
jyyblue <3158733490@qq.com>
2019-12-27 13:00:30 +0300

    [add] refresh 5 min only for mobile

--- Mac Webcaster/CaptureServer.swift
+++ Mac Webcaster/CaptureServer.swift
@@ -165,7 +165,29 @@ public func CaptureServer() -> HttpServer {
                 if( start > 0 && Date.now() - start > 5000 )    // 5s not received
                     location.reload();
             }, 5000);
-            
+
+            function detectmob() {
+             if( navigator.userAgent.match(/Android/i)
+             || navigator.userAgent.match(/webOS/i)
+             || navigator.userAgent.match(/iPhone/i)
+             || navigator.userAgent.match(/iPad/i)
+             || navigator.userAgent.match(/iPod/i)
+             || navigator.userAgent.match(/BlackBerry/i)
+             || navigator.userAgent.match(/Windows Phone/i)
+             ){
+                return true;
+              }
+             else {
+                return false;
+              }
+            }
+            if( detectmob() )
+            {
+                alert('Mobile');
+                setInterval(function() {
+                    location.reload();
+                }, 5 * 60 * 1000);
+            }
         </script>
     </body>
     """ + $0.path)) }
